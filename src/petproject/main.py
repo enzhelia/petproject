f3d7645ca55eea0ec1760048098dc1d4f3afcb54
Хафизова Энже Фаритовна <enzhelia@gmail.com>
2023-03-20 22:08:21 +0300

    add pydantic model and bd models

--- src/petproject/main.py
+++ src/petproject/main.py
@@ -1,15 +1,30 @@
-from typing import Any, Dict, Union
+from typing import List, Union
+from uuid import UUID, uuid4
 
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
-app = FastAPI()
+app = FastAPI(title="App")
+
+item_values = []
 
 
 @app.get("/")
-def read_root() -> Dict[str, str]:
+def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None) -> Dict[str, Any]:
-    return {"item_id": item_id, "q": q}
+@app.get("/nodes/{id}")
+def update_item(id: str):
+    pass
+
+
+# @app.get("/items", response_model=List[Item])
+# def get_all_items():
+#     return item_values
+
+
+# @app.get("/item", response_model=List[Item])
+# def read_item(item_id: UUID):
+#     current_item = list(filter(lambda user: user.item_id == item_id, item_values))
+#     return current_item
---
+++ src/petproject/models/models.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+from sqlalchemy import TIMESTAMP, Column, ForeignKey, Integer, MetaData, String, Table
+
+metadata = MetaData()
+
+
+roles = Table(
+    "folders",
+    metadata,
+    Column("type", String, nullable=False),
+    Column("id", Integer, primary_key=True),
+    Column("size", Integer, nullable=False),
+    Column("parent_id", Integer, nullable=True),
+    Column("date", TIMESTAMP, default=datetime.utcnow),
+)
+
+users = Table(
+    "files",
+    metadata,
+    Column("type", String, nullable=False),
+    Column("id", Integer, primary_key=True),
+    Column("size", Integer, nullable=False),
+    Column("url", String, nullable=False),
+    Column("date", TIMESTAMP, default=datetime.utcnow),
+    Column("parent_id", Integer, ForeignKey("folders.id"), nullable=True),
+)
